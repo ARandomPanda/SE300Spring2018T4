9934540392a59b70a4d49c9a1694196bc9d67866
Christopher McFall <mcfallcl@gmail.com>
2018-02-13 15:11:48 -0500

    Add CourseTest test class
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

---
+++ tests/CourseTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @Test
+    void getNumCredits() {
+    }
+
+    @Test
+    void setNumCredits() {
+    }
+
+    @Test
+    void getGrade() {
+    }
+
+    @Test
+    void setGrade() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setID() {
+    }
+
+    @Test
+    void getPrereqs() {
+    }
+
+    @Test
+    void getCoreqs() {
+    }
+
+    @Test
+    void addPrereq() {
+    }
+
+    @Test
+    void addCoreq() {
+    }
+
+    @Test
+    void removePrereq() {
+    }
+
+    @Test
+    void removeCoreq() {
+    }
+}
