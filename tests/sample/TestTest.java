2e49e3a32590249c51084db96abc5c63de7b0eaa
Christopher McFall <mcfallcl@gmail.com>
2018-02-13 08:11:35 -0500

    Initial commit. Sample code generated by IntelliJ. Create dummy tests to ensure junit works properly.
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

---
+++ tests/sample/TestTest.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testFailTest() {
+        assertFalse(false);
+    }
+}
