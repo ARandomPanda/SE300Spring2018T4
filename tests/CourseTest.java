2624803936b4bf2f5305af7fd583c5e231dc2d17
Christopher McFall <mcfallcl@gmail.com>
2018-02-14 15:56:06 -0500

    Add fail for all tests until implementation.
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

--- tests/CourseTest.java
+++ tests/CourseTest.java
@@ -5,50 +5,67 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourseTest {
 
     @Test
+    void testConstructor() {
+        fail("Unimplemented");
+    }
+
+    @Test
     void getNumCredits() {
+        fail("Unimplemented");
     }
 
     @Test
     void setNumCredits() {
+        fail("Unimplemented");
     }
 
     @Test
     void getGrade() {
+        fail("Unimplemented");
     }
 
     @Test
     void setGrade() {
+        fail("Unimplemented");
     }
 
     @Test
     void setName() {
+        fail("Unimplemented");
     }
 
     @Test
     void setID() {
+        fail("Unimplemented");
     }
 
     @Test
     void getPrereqs() {
+        fail("Unimplemented");
     }
 
     @Test
     void getCoreqs() {
+        fail("Unimplemented");
     }
 
     @Test
     void addPrereq() {
+        fail("Unimplemented");
     }
 
     @Test
     void addCoreq() {
+        fail("Unimplemented");
     }
 
     @Test
     void removePrereq() {
+        fail("Unimplemented");
     }
 
     @Test
     void removeCoreq() {
+        fail("Unimplemented");
     }
 }
