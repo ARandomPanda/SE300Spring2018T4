9b025a08bebfbd1e2a53760dbbc0897cc7ce45b1
Christopher McFall <mcfallcl@gmail.com>
2018-02-14 16:22:45 -0500

    Implement tests for constructor and getNumCredits
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

--- tests/CourseTest.java
+++ tests/CourseTest.java
@@ -1,17 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
 
+    Course testA;
+    Course testB;
+    Course testC;
+
     @Test
     void testConstructor() {
-        fail("Unimplemented");
+        Course a = new Course("AA 101", "TestA", 0);
+        Course b = new Course("BB 101", "TestB", 6);
+        Course c = new Course("CC 101", "TestC", 3);
+        assertThrows(NullPointerException.class,
+                () -> new Course(null, "Test", 0));
+        assertThrows(NullPointerException.class,
+                () -> new Course("Test", null, 0));
+        assertThrows(NullPointerException.class,
+                () -> new Course(null, null, 0));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Course("a", "b", -1));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Course("a", "b", 7));
+    }
+
+    @BeforeEach
+    void makeTestCourse() {
+        testA = new Course("AA 101", "TestA", 0);
+        testB = new Course("BB 101", "TestB", 3);
+        testC = new Course("CC 101", "TestC", 6);
     }
 
     @Test
     void getNumCredits() {
-        fail("Unimplemented");
+        assertEquals(0, testA.getNumCredits());
+        assertEquals(3, testB.getNumCredits());
+        assertEquals(6, testC.getNumCredits());
     }
 
     @Test
