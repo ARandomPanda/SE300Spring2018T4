8dd66e2f83ecbd84304bfa28ec69f1ed46954ad5
Adam Babbit <ambabbit@yahoo.com>
2018-02-14 23:46:51 -0500

    Class MainWindow now creates a visable window

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,9 +1,32 @@
 
-public class MainWindow {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application{
 	
-	public MainWindow()
+	private Stage primaryStage = null;
+	
+	
+	public static void main(String[] args)
 	{
-		System.out.println();
+		launch();
+	}
+	
+	public void start(Stage primaryStage)
+	{
+		this.primaryStage = primaryStage;
+
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("This is a dummy window");
+		
+		this.primaryStage.setScene(new Scene(new BorderPane(), 600, 600));
+		
+		
+		
+		this.primaryStage.show();
+		
 	}
 
 }
