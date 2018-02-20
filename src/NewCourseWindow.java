9f54b9b00bfb6a5cc2f413ca8ff955d6ebaa6d29
Christopher McFall <mcfallcl@gmail.com>
2018-02-19 23:03:48 -0500

    Ensure only a single digit can be input into credits text field
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

--- src/NewCourseWindow.java
+++ src/NewCourseWindow.java
@@ -1,4 +1,6 @@
+import javafx.beans.property.StringProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -7,37 +9,77 @@ import javafx.stage.Stage;
 public class NewCourseWindow {
 
     private static Stage stage = null;
+    private static GridPane grid = new GridPane();
+    private static Scene scene = new Scene(grid, 400, 300);
+    private static Label IDLabel = new Label("Course ID: ");
+    private static TextField IDField = new TextField();
+    private static Label nameLabel = new Label("Course Name: ");
+    private static TextField nameField = new TextField();
+    private static Label creditsLabel = new Label("Credits: ");
+    private static TextField creditsField = new TextField();
+
+    private static Button okButton = new Button("Add Course");
+    private static Button cancelButton = new Button("Cancel");
 
     private NewCourseWindow() { }
 
     // Testing only
     public static void set(Stage s) {
         stage = s;
+        initInputSanitizers();
+        grid.addColumn(0, IDLabel, nameLabel, creditsLabel, cancelButton);
+        grid.addColumn(1, IDField, nameField, creditsField, okButton);
+        stage.setScene(scene);
     }
 
     public static void init() {
-        if (stage == null) stage = new Stage();
-
-        stage.setScene(initScene());
+        if (stage == null) {
+            stage = new Stage();
+            initInputSanitizers();
+            grid.addColumn(0, IDLabel, nameLabel, creditsLabel, cancelButton);
+            grid.addColumn(1, IDField, nameField, creditsField, okButton);
+            stage.setScene(scene);
+        }
     }
 
     public static void show() {
+        resetScene();
         stage.show();
     }
 
-    private static Scene initScene() {
-        GridPane grid = new GridPane();
-        Scene scene = new Scene(grid, 400, 300);
-        Label IDLabel = new Label("Course ID: ");
-        TextField IDField = new TextField();
-        Label nameLabel = new Label("Course Name: ");
-        TextField nameField = new TextField();
-        Label creditsLabel = new Label("Credits: ");
-        TextField creditsField = new TextField();
+    public static void hide() {
+        stage.hide();
+    }
 
-        grid.addColumn(0, IDLabel, nameLabel, creditsLabel);
-        grid.addColumn(1, IDField, nameField, creditsField);
+    private static void resetScene() {
+        IDField.clear();
+        nameField.clear();
+        creditsField.clear();
+    }
 
-        return scene;
+    private static void initInputSanitizers() {
+        initCreditSanitizer();
+        initIDSanitizer();
+        initNameSanitizer();
+    }
+
+    private static void initCreditSanitizer() {
+        creditsField.textProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if (newValue.isEmpty() || newValue.matches("\\d")) {
+                        ((StringProperty) observable).setValue(newValue);
+                    } else {
+                        ((StringProperty) observable).setValue(oldValue);
+                    }
+                }
+        );
+    }
+
+    private static void initIDSanitizer() {
+
+    }
+
+    private static void initNameSanitizer() {
+
     }
 }
