54893d0395f9bb60d3242428bb7dbe2e39dc924e
Christopher McFall <mcfallcl@gmail.com>
2018-02-20 12:42:24 -0500

    Add MasterCourseList class
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

---
+++ src/MasterCourseList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MasterCourseList {
+
+    // update to use a file.
+    private static final String fileName = "";
+
+    private static ArrayList<Course> courseList;
+
+    private MasterCourseList() {
+
+    }
+
+    public static void init() {
+        if (courseList == null) {
+            courseList = new ArrayList<>();
+        }
+    }
+
+    public static void addCourse(Course course) {
+        courseList.add(course);
+    }
+
+    public static List<Course> getCourseList() {
+        return Collections.unmodifiableList(courseList);
+    }
+}
