428fd63b3896c2410283ed236a16cbf675fef34d
Adam Babbit <ambabbit@yahoo.com>
2018-02-26 15:19:40 -0500

    Commented the MainWindow.java file

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 public class MainWindow extends Application{
 	
+	// Instantiates the main window for the program 
 	private Stage primaryStage = null;
 	
 	
@@ -16,6 +17,8 @@ public class MainWindow extends Application{
 		launch();
 	}
 	
+	
+	// Standard start program required to use JavaFX
 	public void start(Stage primaryStage)
 	{
 		this.primaryStage = primaryStage;
@@ -29,6 +32,12 @@ public class MainWindow extends Application{
 		
 	}
 	
+	/**
+	 * Creates the Standard first scene of the program. Displays the first Window 
+	 * including the Semesters, Classes, Class pool, and toolbars
+	 * 
+	 * @return scene		The Scene in which the other scenes are stored, This should be added directly to the Stage
+	 */
 	private Scene setupScene()
 	{
 		BorderPane highestPane = new BorderPane();
@@ -42,6 +51,11 @@ public class MainWindow extends Application{
 		return scene;
 	}
 	
+	/**
+	 * Creates the frame for the new menu
+	 * 
+	 * @return toolbar		The menu bar to be added in the window.
+	 */
 	private MenuBar makeMenu()
 	{
 		MenuBar toolBar = new MenuBar();
