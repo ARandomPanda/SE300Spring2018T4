8127fbbd49bba96714eeaa1f294b2a43211957fb
Adam Babbit <ambabbit@yahoo.com>
2018-02-28 16:18:10 -0500

    Implemented the MasterCourseListWindow to the Menu

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,23 +1,16 @@
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application{
 	
 	// Instantiates the main window for the program 
 	private Stage primaryStage = null;
-	private Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
 	
 	
 	public static void main(String[] args)
@@ -27,19 +20,14 @@ public class MainWindow extends Application{
 	
 	
 	// Standard start program required to use JavaFX
-	public void start(Stage stage)
+	public void start(Stage primaryStage)
 	{
-		primaryStage = stage;
+		this.primaryStage = primaryStage;
+
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("This is a dummy window");
 		
-		primaryStage.setTitle("Curriculum Map");
-		
-		primaryStage.setX(screenSize.getMinX());
-		primaryStage.setY(screenSize.getMinY());
-		primaryStage.setHeight(screenSize.getHeight());
-		primaryStage.setWidth(screenSize.getWidth());
-		primaryStage.setMinHeight(300);
-		primaryStage.setMinWidth(500);
-		primaryStage.setScene(setupScene());
+		this.primaryStage.setScene(setupScene());
 		
 		this.primaryStage.show();
 		
@@ -53,73 +41,33 @@ public class MainWindow extends Application{
 	 */
 	private Scene setupScene()
 	{
-		// the Highest level pane that contains all the other panes
 		BorderPane highestPane = new BorderPane();
 		BorderPane topBorderPane = new BorderPane();
-		Scene scene = new Scene(highestPane);
 		
 		topBorderPane.setTop(makeMenu());
 		highestPane.setTop(topBorderPane);
-		highestPane.setBottom(setUpSemesterPane());
+		
+		Scene scene = new Scene(highestPane, 600, 600);
 		
 		return scene;
-	}
-	
-	private GridPane setUpSemesterPane()
-	{
-		GridPane semesterGrid = new GridPane();
-		for (int i = 0; i < 8; i++)
-		{
-			Pane semesterPane = new Pane();
-			Label l = new Label("This is a Semester");
-			
-			semesterPane.prefWidthProperty().bind(primaryStage.widthProperty().multiply(.3333));
-		
-			semesterPane.getChildren().add(l);
-			semesterGrid.add(semesterPane, i+1, 0);
-		}
-		
-		// TODO Outdated code, need to be removed on final version
-		
-		/*Pane p1 = new Pane();
-		Pane p2 = new Pane();
-		Pane p3 = new Pane();
-		
-		p1.prefWidthProperty().bind(primaryStage.widthProperty().multiply(.3333));
-		p2.prefWidthProperty().bind(primaryStage.widthProperty().multiply(.3333));
-		p3.prefWidthProperty().bind(primaryStage.widthProperty().multiply(.3333));
-		
-		p1.setStyle("-fx-border-color: black");
-		p2.setStyle("-fx-border-color: red");
-		p3.setStyle("-fx-border-color: blue");
-		
-		semesterGrid.add(p1, 1, 0);
-		semesterGrid.add(p2, 2, 0);
-		semesterGrid.add(p3, 3, 0);*/
-
-		semesterGrid.prefHeightProperty().bind(primaryStage.heightProperty().multiply(.6666));
-		semesterGrid.prefWidthProperty().bind(primaryStage.widthProperty());
-		
-		return semesterGrid;
 	}
 	
 	/**
 	 * Creates the frame for the new menu
 	 * 
-	 * @return toolbar	The menu bar to be added in the window.
+	 * @return toolbar		The menu bar to be added in the window.
 	 */
 	private MenuBar makeMenu()
 	{
-		MenuBar toolBar = new ApplicationMenu().getMenuBar();
-		MenuItem newCourse = new MenuItem("Display Master Course List");
-		Menu toAdd = new Menu("To Open List");
+		MenuBar toolBar = new MenuBar();
+		Menu dummy = new Menu("This is a Test");
+		MenuItem newCourse = new MenuItem("Opens Course List");
 		
-		toAdd.getItems().add(newCourse);
 		
 		newCourse.setOnAction(e -> {
 			MasterCourseListWindow.startAsChild(primaryStage);});
-		
-		toolBar.getMenus().add(toAdd);
+		dummy.getItems().add(newCourse);
+		toolBar.getMenus().add(dummy);
 		
 		return toolBar;
 	}
