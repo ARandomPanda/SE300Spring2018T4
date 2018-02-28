dee18c9147bfba925556191d1f82bd8b99a5bb38
Nguyenj93 <jnguyen9372@gmail.com>
2018-02-28 15:40:30 -0500

    Add Semester class

---
+++ src/Semester.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Semester{
+    private string term;
+    private int year;
+    private ArrayList semester = new ArrayList();
+
+    public setTerm(string term){
+        this.term = term;
+    }
+
+    public setYear( int year){
+        this.year = year;
+    }
+
+    public ArrayList setCourse(ArrayList course){
+        semester = add.course
+    }
+
+    public getTerm{
+        return this.term;
+    }
+
+    public getYear{
+        return this.year;
+    }
+
+    public ArrayList getCourse{
+        return semester;
+    }
