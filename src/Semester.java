dbd13b68dd63ae0564ef6f65e409296e4ca5c73c
Nguyenj93 <jnguyen9372@gmail.com>
2018-03-06 17:27:30 -0500

    Still hate github

--- src/Semester.java
+++ src/Semester.java
@@ -1,31 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Semester{
-    private string term;
+// push comment
+public class Semester {
+    private String term;
     private int year;
-    private ArrayList semester = new ArrayList();
+    private ArrayList<RequiredCourse> semesterClass = new ArrayList();
 
-    public setTerm(string term){
+    public void setTerm(String term) {
         this.term = term;
     }
 
-    public setYear( int year){
+    public void setYear(int year) {
         this.year = year;
     }
 
-    public ArrayList setCourse(ArrayList course){
-        semester = add.course
+    public void setCourse(BaseCourse course, Grade grade) {
+        semesterClass.add(new RequiredCourse(course, grade));
     }
 
-    public getTerm{
-        return this.term;
+    public String getTerm() {
+        return term;
     }
 
-    public getYear{
-        return this.year;
+    public int getYear() {
+        return year;
     }
 
-    public ArrayList getCourse{
-        return semester;
+    public List<RequiredCourse> getCourse() {
+        return semesterClass;
     }
+}
