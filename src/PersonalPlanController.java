6b3c0da26ea0f787e44a664c1487fb3eab6a128c
Christopher McFall <mcfallcl@gmail.com>
2018-03-25 10:43:47 -0400

    Add PersonalPlanController and methods

---
+++ src/PersonalPlanController.java
@@ -0,0 +1,40 @@
+import java.io.File;
+
+public class PersonalPlanController {
+
+    private static PersonalPlan plan;
+
+    private static File planSaveFile;
+
+    public static void savePersonalPlan() {
+
+    }
+
+    public static void savePersonalPlan(String filename) {
+
+    }
+
+    public static void newPersonalPlan() {
+
+    }
+
+    public static void loadPersonalPlan() {
+
+    }
+
+    public static void addSemester() {
+
+    }
+
+    public static void removeSemester(Semester semester) {
+
+    }
+
+    public static void addCourse(BaseCourse course, Semester semester) {
+
+    }
+
+    public static void removeCourse(BaseCourse course, Semester semester) {
+
+    }
+}
