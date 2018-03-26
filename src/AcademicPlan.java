800531eee19fc37f0ba0d93efa6da7f51ad3de4f
tyler <tyler@f00bar.com>
2018-03-26 16:54:11 -0400

    added setters and getters to academic plan

--- src/AcademicPlan.java
+++ src/AcademicPlan.java
@@ -1,6 +1,8 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -12,30 +14,31 @@ public class AcademicPlan implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private ArrayList<DegreeProgram> degrees;
-	@SuppressWarnings("unused")
-	private String name;
-	@SuppressWarnings("unused")
 	private short credits;
-	@SuppressWarnings("unused")
 	private double GPA;
+	private String fileLocation, catalogYear;
+	private ArrayList<String> degreeTitles;
 	
 	AcademicPlan() {
 		degrees = new ArrayList<DegreeProgram>();
-		credits = 0;
-		GPA = 0.0;
-	}
-	
-	AcademicPlan(ArrayList<DegreeProgram> listOfDegrees) {
-		this.degrees = listOfDegrees;
-		credits = 0;
-		GPA = 0.0;
+		initializeConstants();
 	}
 
-	AcademicPlan(ArrayList<DegreeProgram> listOfDegrees, String name) {
+	AcademicPlan(DegreeProgram degree) {
+		this.degrees.add(degree);
+		initializeConstants();
+	}
+/* TODO replace this constructor by extracting the catalog year, title, etc. from the degree program object
+	AcademicPlan(ArrayList<DegreeProgram> listOfDegrees, String degreeTitle) {
 		this.degrees = listOfDegrees;
-		this.name = name;
+		this.addDegreeTitle(degreeTitle);
+		initializeConstants();
+	}
+*/
+	private void initializeConstants() {
 		credits = 0;
 		GPA = 0.0;
+		fileLocation = "assets/academicPlan.obj";
 	}
 
 	/**
@@ -59,22 +62,67 @@ public class AcademicPlan implements Serializable{
 	public boolean savePlan() {
 		FileOutputStream fOut = null;
 		ObjectOutputStream oos = null;
-		String current_directory = System.getProperty("user_dir");
 		
-		try { //TODO complete to save academic plan
-			fOut = new FileOutputStream(current_directory);
+		try {
+			fOut = new FileOutputStream(fileLocation);
 			oos = new ObjectOutputStream(fOut);
 			oos.writeObject(this);
-			
+			oos.close();
+			fOut.close();
+			return true;
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("The file doesn't exist for academic plan.");
+			//e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("The academic plan object can't be saved.");
 			e.printStackTrace();
-		} finally {
-			
 		}
 		return false;
+	}
+	
+	public boolean loadPlan() {
+		FileInputStream fIn = null;
+		ObjectInputStream ois = null;
+		
+		try {
+			fIn = new FileInputStream(fileLocation);
+			ois = new ObjectInputStream(fIn);
+			ois.readObject();
+			ois.close();
+			fIn.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			System.out.println("The file doesn't exist for academic plan.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("The academic plan object is missing from '" + fileLocation + "'.");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("The academic plan object may be corrupted, causing a failure to load academic plan object.");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public double calculateGPA() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int calculateCredits() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void setCatalogYear(String year) {
+		this.catalogYear = year;
+	}
+	
+	private boolean addDegreeTitle(String title) {
+		return this.degreeTitles.add(title);
+	}
+	
+	private boolean removeDegreeTitle(String title) {
+		return this.degreeTitles.remove(title);
 	}
 }
