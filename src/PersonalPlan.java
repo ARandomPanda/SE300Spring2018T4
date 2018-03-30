e9de4077be462b9da131f7f1a42329af9c5a8ef5
Christopher McFall <mcfallcl@gmail.com>
2018-03-30 09:51:31 -0400

    Remove unused code

--- src/PersonalPlan.java
+++ src/PersonalPlan.java
@@ -7,37 +7,9 @@ import java.util.Collections;
 
 public class PersonalPlan {
 
-    private int catalogYear;
-    private String program;
-
     private ArrayList<Semester> semesters = new ArrayList<>();
 
-    private ArrayList<BaseCourse> requiredClass = new ArrayList<>();
-
-
-    PersonalPlan() {
-    }
-
-    public void setProgram(String program) {
-        this.program = program;
-    }
-
-    public void setCatalogYear(int catalogYear) {
-        this.catalogYear = catalogYear;
-    }
-
-
-    public void setRequiredClass(BaseCourse course) {
-        requiredClass.add(course);
-    }
-
-    public ObservableList<BaseCourse> getRequiredClass() {
-        return FXCollections.unmodifiableObservableList(FXCollections.observableList(requiredClass));
-    }
-
-    public String getProgram() {
-        return program;
-    }
+    PersonalPlan() { }
 
     public void addSemester(Semester semester){
         semesters.add(semester);
@@ -50,10 +22,6 @@ public class PersonalPlan {
 
     public ObservableList<Semester> getSemesters() {
         return FXCollections.unmodifiableObservableList(FXCollections.observableList(semesters));
-    }
-
-    public int getCatalogYear() {
-        return catalogYear;
     }
 
 }
